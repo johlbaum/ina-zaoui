fd4eb88f95806a3f546178464c7c2fba46f4aace
johlbaum <julien.ohlbaum@gmail.com>
2025-01-03 12:24:23 +0100

    File upload validation: Check file type and size.

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\MediaRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MediaRepository::class)]
 class Media
@@ -26,6 +27,11 @@ class Media
     #[ORM\Column]
     private string $title;
 
+    #[Assert\File(
+        maxSize: '2M',
+        mimeTypes: ['image/jpeg', 'image/png'],
+        mimeTypesMessage: 'Le fichier doit être une image au format JPEG ou PNG.'
+    )]
     private ?UploadedFile $file = null;
 
     public function getId(): ?int
